0a79bbb4eca4fe15f1f542616414bb8b211e922a
baosuping <73763262+baosuping@users.noreply.github.com>
2023-02-26 17:34:54 +0800

    Add files via upload

---
+++ models/model.py
@@ -0,0 +1,347 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from utils.masking import TriangularCausalMask, ProbMask
+from models.encoder import Encoder, EncoderLayer, newEncoder, newEncoderLayer, ConvLayer, EncoderStack, my_Layernorm, series_decomp
+from models.decoder import Decoder, DecoderLayer, newDecoder, newDecoderLayer
+from models.attn import FullAttention, ProbAttention, AttentionLayer
+from models.embed import DataEmbedding
+
+import math
+import numpy as np
+
+class Informer(nn.Module):
+    def __init__(self, enc_in, dec_in, c_out, seq_len, label_len, out_len, 
+                factor=5, d_model=512, n_heads=8, e_layers=3, d_layers=2, d_ff=512, 
+                dropout=0.0, attn='prob', embed='fixed', freq='h', activation='gelu', 
+                output_attention = False, distil=True, mix=True,
+                device=torch.device('cuda:0')):
+        super(Informer, self).__init__()
+        self.pred_len = out_len
+        self.attn = attn
+        self.output_attention = output_attention
+
+        # Encoding
+        self.enc_embedding = DataEmbedding(enc_in, d_model, embed, freq, dropout)
+        self.dec_embedding = DataEmbedding(dec_in, d_model, embed, freq, dropout)
+        # Attention
+        Attn = ProbAttention if attn=='prob' else FullAttention
+        # Encoder
+        self.encoder = Encoder(
+            [
+                EncoderLayer(
+                    AttentionLayer(Attn(False, factor, attention_dropout=dropout, output_attention=output_attention), 
+                                d_model, n_heads, mix=False),
+                    d_model,
+                    d_ff,
+                    dropout=dropout,
+                    activation=activation
+                ) for l in range(e_layers)
+            ],
+            [
+                ConvLayer(
+                    d_model
+                ) for l in range(e_layers-1)
+            ] if distil else None,
+            norm_layer=torch.nn.LayerNorm(d_model)
+        )
+        # Decoder
+        self.decoder = Decoder(
+            [
+                DecoderLayer(
+                    AttentionLayer(Attn(True, factor, attention_dropout=dropout, output_attention=False), 
+                                d_model, n_heads, mix=mix),
+                    AttentionLayer(FullAttention(False, factor, attention_dropout=dropout, output_attention=False), 
+                                d_model, n_heads, mix=False),
+                    d_model,
+                    d_ff,
+                    dropout=dropout,
+                    activation=activation,
+                )
+                for l in range(d_layers)
+            ],
+            norm_layer=torch.nn.LayerNorm(d_model)
+        )
+        # self.end_conv1 = nn.Conv1d(in_channels=label_len+out_len, out_channels=out_len, kernel_size=1, bias=True)
+        # self.end_conv2 = nn.Conv1d(in_channels=d_model, out_channels=c_out, kernel_size=1, bias=True)
+        self.projection = nn.Linear(d_model, c_out, bias=True)
+        
+    def forward(self, x_enc, x_mark_enc, x_dec, x_mark_dec, 
+                enc_self_mask=None, dec_self_mask=None, dec_enc_mask=None):
+        enc_out = self.enc_embedding(x_enc, x_mark_enc)
+        enc_out, attns = self.encoder(enc_out, attn_mask=enc_self_mask)
+
+        dec_out = self.dec_embedding(x_dec, x_mark_dec)
+        dec_out = self.decoder(dec_out, enc_out, x_mask=dec_self_mask, cross_mask=dec_enc_mask)
+        dec_out = self.projection(dec_out)
+        
+        # dec_out = self.end_conv1(dec_out)
+        # dec_out = self.end_conv2(dec_out.transpose(2,1)).transpose(1,2)
+        if self.output_attention:
+            return dec_out[:,-self.pred_len:,:], attns
+        else:
+            return dec_out[:,-self.pred_len:,:] # [B, L, D]
+
+
+class STformer(nn.Module):
+
+    def __init__(self, enc_in, dec_in, c_out, seq_len, label_len, out_len, 
+                factor=5, d_model=512, n_heads=8, e_layers=3, d_layers=2, d_ff=512, 
+                dropout=0.0, attn='prob', embed='fixed', freq='h', activation='gelu', 
+                output_attention = False, distil=True, mix=True,moving_avg = 1,
+                device=torch.device('cuda:0')):
+        super(STformer, self).__init__()
+        self.seq_len = seq_len # different
+        self.pred_len = out_len
+        self.label_len = label_len
+        self.attn = attn
+        self.output_attention = output_attention
+
+        # Decomp
+        kernel_size = moving_avg
+        self.decomp = series_decomp(kernel_size)
+
+        # Encoding
+        self.enc_embedding = DataEmbedding(enc_in, d_model, embed, freq, dropout)
+        self.dec_embedding = DataEmbedding(dec_in, d_model, embed, freq, dropout)
+        # Attention
+        Attn = ProbAttention if attn=='prob' else FullAttention
+        # Encoder
+        self.encoder = newEncoder(
+            [
+                newEncoderLayer(
+                    AttentionLayer(Attn(False, factor, attention_dropout=dropout, output_attention=output_attention), 
+                                d_model, n_heads, mix=False),
+                    d_model,
+                    d_ff,
+                    moving_avg=moving_avg,
+                    dropout=dropout,
+                    activation=activation
+                ) for l in range(len(e_layers))
+            ],
+            # [
+            #     ConvLayer(
+            #         d_model
+            #     ) for l in range(e_layers-1)
+            # ] if distil else None,
+            norm_layer=my_Layernorm(d_model)
+        )
+
+
+        # Decoder
+        self.decoder = newDecoder(
+            [
+                newDecoderLayer(
+                    AttentionLayer(Attn(True, factor, attention_dropout=dropout, output_attention=False), 
+                                d_model, n_heads, mix=mix),
+                    AttentionLayer(FullAttention(False, factor, attention_dropout=dropout, output_attention=False), 
+                                d_model, n_heads, mix=False),
+                    d_model,
+                    c_out,
+                    d_ff,
+                    moving_avg=moving_avg,
+                    dropout=dropout,
+                    activation=activation,
+                )
+                for l in range(d_layers)
+            ],
+            norm_layer=my_Layernorm(d_model),
+            projection=nn.Linear(d_model, c_out, bias=True)
+        )
+        # self.end_conv1 = nn.Conv1d(in_channels=label_len+out_len, out_channels=out_len, kernel_size=1, bias=True)
+        # self.end_conv2 = nn.Conv1d(in_channels=d_model, out_channels=c_out, kernel_size=1, bias=True)
+        # self.projection = nn.Linear(d_model, c_out, bias=True)
+
+    def forward(self, x_enc, x_mark_enc, x_dec, x_mark_dec,
+                enc_self_mask=None, dec_self_mask=None, dec_enc_mask=None):
+        # decomp init
+        mean = torch.mean(x_enc, dim=1).unsqueeze(1).repeat(1, self.pred_len, 1)
+        zeros = torch.zeros([x_dec.shape[0], self.pred_len, x_dec.shape[2]], device=x_enc.device)
+        seasonal_init, trend_init = self.decomp(x_enc)
+        # decoder input
+        trend_init = torch.cat([trend_init[:, -self.label_len:, :], mean], dim=1)
+        seasonal_init = torch.cat([seasonal_init[:, -self.label_len:, :], zeros], dim=1)
+        # enc
+        enc_out = self.enc_embedding(x_enc, x_mark_enc)
+        enc_out, attns = self.encoder(enc_out, attn_mask=enc_self_mask)
+        # dec
+        dec_out = self.dec_embedding(seasonal_init, x_mark_dec)
+        seasonal_part, trend_part = self.decoder(dec_out, enc_out, x_mask=dec_self_mask, cross_mask=dec_enc_mask,
+                                                 trend=trend_init)
+        # final
+        dec_out = trend_part + seasonal_part
+
+        if self.output_attention:
+            return dec_out[:, -self.pred_len:, :], attns
+        else:
+            return dec_out[:, -self.pred_len:, :]  # [B, L, D]
+
+
+class InformerStack(nn.Module):
+    def __init__(self, enc_in, dec_in, c_out, seq_len, label_len, out_len, 
+                factor=5, d_model=512, n_heads=8, e_layers=[3,2,1], d_layers=2, d_ff=512, 
+                dropout=0.0, attn='prob', embed='fixed', freq='h', activation='gelu',
+                output_attention = False, distil=True, mix=True,
+                device=torch.device('cuda:0')):
+        super(InformerStack, self).__init__()
+        self.pred_len = out_len
+        self.attn = attn
+        self.output_attention = output_attention
+
+        # Encoding
+        self.enc_embedding = DataEmbedding(enc_in, d_model, embed, freq, dropout)
+        self.dec_embedding = DataEmbedding(dec_in, d_model, embed, freq, dropout)
+        # Attention
+        Attn = ProbAttention if attn=='prob' else FullAttention
+        # Encoder
+
+        inp_lens = list(range(len(e_layers))) # [0,1,2,...] you can customize here
+        encoders = [
+            Encoder(
+                [
+                    EncoderLayer(
+                        AttentionLayer(Attn(False, factor, attention_dropout=dropout, output_attention=output_attention), 
+                                    d_model, n_heads, mix=False),
+                        d_model,
+                        d_ff,
+                        dropout=dropout,
+                        activation=activation
+                    ) for l in range(el)
+                ],
+                [
+                    ConvLayer(
+                        d_model
+                    ) for l in range(el-1)
+                ] if distil else None,
+                norm_layer=torch.nn.LayerNorm(d_model)
+            ) for el in e_layers]
+        self.encoder = EncoderStack(encoders, inp_lens)
+        # Decoder
+        self.decoder = Decoder(
+            [
+                DecoderLayer(
+                    AttentionLayer(Attn(True, factor, attention_dropout=dropout, output_attention=False), 
+                                d_model, n_heads, mix=mix),
+                    AttentionLayer(FullAttention(False, factor, attention_dropout=dropout, output_attention=False), 
+                                d_model, n_heads, mix=False),
+                    d_model,
+                    d_ff,
+                    dropout=dropout,
+                    activation=activation,
+                )
+                for l in range(d_layers)
+            ],
+            norm_layer=torch.nn.LayerNorm(d_model)
+        )
+        # self.end_conv1 = nn.Conv1d(in_channels=label_len+out_len, out_channels=out_len, kernel_size=1, bias=True)
+        # self.end_conv2 = nn.Conv1d(in_channels=d_model, out_channels=c_out, kernel_size=1, bias=True)
+        self.projection = nn.Linear(d_model, c_out, bias=True)
+        
+    def forward(self, x_enc, x_mark_enc, x_dec, x_mark_dec, 
+                enc_self_mask=None, dec_self_mask=None, dec_enc_mask=None):
+        enc_out = self.enc_embedding(x_enc, x_mark_enc)
+        enc_out, attns = self.encoder(enc_out, attn_mask=enc_self_mask)
+
+        dec_out = self.dec_embedding(x_dec, x_mark_dec)
+        dec_out = self.decoder(dec_out, enc_out, x_mask=dec_self_mask, cross_mask=dec_enc_mask)
+        dec_out = self.projection(dec_out)
+        
+        # dec_out = self.end_conv1(dec_out)
+        # dec_out = self.end_conv2(dec_out.transpose(2,1)).transpose(1,2)
+        if self.output_attention:
+            return dec_out[:,-self.pred_len:,:], attns
+        else:
+            return dec_out[:,-self.pred_len:,:] # [B, L, D]
+
+            
+# class Autoformer(nn.Module):
+#     """
+#     Autoformer is the first method to achieve the series-wise connection,
+#     with inherent O(LlogL) complexity
+#     """
+#     def __init__(self, configs):
+#         super(Autoformer, self).__init__()
+#         self.seq_len = configs.seq_len
+#         self.label_len = configs.label_len
+#         self.pred_len = configs.pred_len
+#         self.output_attention = configs.output_attention
+
+#         # Decomp
+#         kernel_size = configs.moving_avg
+#         self.decomp = series_decomp(kernel_size)
+
+#         # Embedding
+#         # The series-wise connection inherently contains the sequential information.
+#         # Thus, we can discard the position embedding of transformers.
+#         self.enc_embedding = DataEmbedding_wo_pos(configs.enc_in, configs.d_model, configs.embed, configs.freq,
+#                                                   configs.dropout)
+#         self.dec_embedding = DataEmbedding_wo_pos(configs.dec_in, configs.d_model, configs.embed, configs.freq,
+#                                                   configs.dropout)
+
+#         # Encoder
+#         self.encoder = Encoder(
+#             [
+#                 EncoderLayer(
+#                     AutoCorrelationLayer(
+#                         AutoCorrelation(False, configs.factor, attention_dropout=configs.dropout,
+#                                         output_attention=configs.output_attention),
+#                         configs.d_model, configs.n_heads),
+#                     configs.d_model,
+#                     configs.d_ff,
+#                     moving_avg=configs.moving_avg,
+#                     dropout=configs.dropout,
+#                     activation=configs.activation
+#                 ) for l in range(configs.e_layers)
+#             ],
+#             norm_layer=my_Layernorm(configs.d_model)
+#         )
+#         # Decoder
+#         self.decoder = Decoder(
+#             [
+#                 DecoderLayer(
+#                     AutoCorrelationLayer(
+#                         AutoCorrelation(True, configs.factor, attention_dropout=configs.dropout,
+#                                         output_attention=False),
+#                         configs.d_model, configs.n_heads),
+#                     AutoCorrelationLayer(
+#                         AutoCorrelation(False, configs.factor, attention_dropout=configs.dropout,
+#                                         output_attention=False),
+#                         configs.d_model, configs.n_heads),
+#                     configs.d_model,
+#                     configs.c_out,
+#                     configs.d_ff,
+#                     moving_avg=configs.moving_avg,
+#                     dropout=configs.dropout,
+#                     activation=configs.activation,
+#                 )
+#                 for l in range(configs.d_layers)
+#             ],
+#             norm_layer=my_Layernorm(configs.d_model),
+#             projection=nn.Linear(configs.d_model, configs.c_out, bias=True)
+#         )
+
+#     def forward(self, x_enc, x_mark_enc, x_dec, x_mark_dec,
+#                 enc_self_mask=None, dec_self_mask=None, dec_enc_mask=None):
+#         # decomp init
+#         mean = torch.mean(x_enc, dim=1).unsqueeze(1).repeat(1, self.pred_len, 1)
+#         zeros = torch.zeros([x_dec.shape[0], self.pred_len, x_dec.shape[2]], device=x_enc.device)
+#         seasonal_init, trend_init = self.decomp(x_enc)
+#         # decoder input
+#         trend_init = torch.cat([trend_init[:, -self.label_len:, :], mean], dim=1)
+#         seasonal_init = torch.cat([seasonal_init[:, -self.label_len:, :], zeros], dim=1)
+#         # enc
+#         enc_out = self.enc_embedding(x_enc, x_mark_enc)
+#         enc_out, attns = self.encoder(enc_out, attn_mask=enc_self_mask)
+#         # dec
+#         dec_out = self.dec_embedding(seasonal_init, x_mark_dec)
+#         seasonal_part, trend_part = self.decoder(dec_out, enc_out, x_mask=dec_self_mask, cross_mask=dec_enc_mask,
+#                                                  trend=trend_init)
+#         # final
+#         dec_out = trend_part + seasonal_part
+
+#         if self.output_attention:
+#             return dec_out[:, -self.pred_len:, :], attns
+#         else:
+#             return dec_out[:, -self.pred_len:, :]  # [B, L, D]
+
