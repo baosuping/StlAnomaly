57c6c0670e22580423d308ce1ca0059fe16f8299
baosuping <73763262+baosuping@users.noreply.github.com>
2023-04-14 17:15:44 +0800

    Update data_loader.py

--- data/data_loader.py
+++ data/data_loader.py
@@ -512,7 +512,107 @@ class Dataset_yahoo_Anomaly(Dataset):
     def inverse_transform(self, data):
         return self.scaler.inverse_transform(data)
 
+class Dataset_synthetic_Anomaly(Dataset):
+    def __init__(self, root_path, flag='train', size=None,
+                 features='S', data_path='collective_global_0.1.csv',
+                 target='value', scale=True, inverse=False, timeenc=0, freq='s', cols=None):
+        # size [seq_len, label_len, pred_len]
+        # info
+        if size == None:
+            self.seq_len = 24 * 4 * 4
+            self.label_len = 24 * 4
+            self.pred_len = 24 * 4
+        else:
+            self.seq_len = size[0]
+            self.label_len = size[1]
+            self.pred_len = size[2]
+        # init
+        assert flag in ['train', 'test', 'val']
+        type_map = {'train': 0, 'val': 1, 'test': 2}
+        self.set_type = type_map[flag]
 
+        self.features = features
+        self.target = target
+        self.scale = scale
+        self.inverse = inverse
+        self.timeenc = timeenc
+        self.freq = freq
+
+        self.root_path = root_path
+        self.data_path = data_path
+        self.__read_data__()
+
+    def __read_data__(self):
+        self.scaler = StandardScaler()
+        # df_raw = pd.read_csv(os.path.join(self.root_path,
+        #                                   self.data_path))
+
+        # 修改  用分号隔开
+        path = os.path.join(self.root_path, self.data_path)
+
+        df_raw = pd.read_csv(path, sep=',', parse_dates=True)
+
+        # 修改
+        # type_map = {'train': 0, 'val': 1, 'test': 2}
+        border1s = [0, 0, 0]
+        border2s = [80, 80, len(df_raw)]
+        border1 = border1s[self.set_type]
+        border2 = border2s[self.set_type]
+
+        if self.features == 'M' or self.features == 'MS':
+            ## 去掉时间列以及后面一列
+            cols_data = df_raw.columns[1: -1]
+            df_data = df_raw[cols_data]
+        elif self.features == 'S':
+            df_data = df_raw[[self.target]]
+
+        ## 归一化
+        if self.scale:
+            train_data = df_data[border1s[0]:border2s[0]]
+            self.scaler.fit(train_data.values)
+            data = self.scaler.transform(df_data.values)
+        else:
+            data = df_data.values
+
+        # 修改   根据csv的时间列名
+        df_stamp = df_raw[['timestamp']][border1:border2]
+        df_stamp['date'] = pd.to_datetime(df_stamp.timestamp)
+        ## 'date'与timefeatures保持一致
+        ## time embedding
+        data_stamp = time_features(df_stamp, timeenc=self.timeenc, freq=self.freq)
+
+        self.data_x = data[border1:border2]
+        if self.inverse:
+            self.data_y = df_data.values[border1:border2]
+        else:
+            self.data_y = data[border1:border2]
+        self.data_stamp = data_stamp
+
+    def __getitem__(self, index):
+        s_begin = index
+        s_end = s_begin + self.seq_len
+        r_begin = s_end - self.label_len
+        r_end = r_begin + self.label_len + self.pred_len
+
+        ## Encoder的输入
+        seq_x = self.data_x[s_begin:s_end]
+        if self.inverse:
+            seq_y = np.concatenate(
+                [self.data_x[r_begin:r_begin + self.label_len], self.data_y[r_begin + self.label_len:r_end]], 0)
+        else:
+            ## Decoder的输入
+            seq_y = self.data_y[r_begin:r_end]
+        seq_x_mark = self.data_stamp[s_begin:s_end]
+        seq_y_mark = self.data_stamp[r_begin:r_end]
+
+        return seq_x, seq_y, seq_x_mark, seq_y_mark
+
+    def __len__(self):
+        ## 实际模型训练的数据长度 1147-30-1+1=1117
+        return len(self.data_x) - self.seq_len - self.pred_len + 1
+
+    def inverse_transform(self, data):
+        return self.scaler.inverse_transform(data)
 # class Dataset_synthetic_Anomaly(Dataset):
 #     def __init__(self, root_path, flag='train', size=None,
 #                  features='M', data_path='learningData.csv',
